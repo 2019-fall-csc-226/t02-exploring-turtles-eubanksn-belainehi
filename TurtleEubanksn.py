7434fe6c21beeaa27bbad62c1c9b85155b5e66fb
eubanksn <eubanksn@berea.edu>
2019-09-04 10:19:05 -0400

    Finished

--- TurtleEubanksn.py
+++ TurtleEubanksn.py
@@ -8,5 +8,28 @@ smallboi.shape("turtle")
 smallboi.color("black")
 #everything to set up turtle and bg
 ###################################
-
-
+smallboi.pensize(10)
+smallboi.forward(50)
+smallboi.right(90)
+smallboi.forward(50)
+smallboi.left(180)
+smallboi.forward(100)
+smallboi.right(180)
+smallboi.forward(50)
+smallboi.right(90)
+smallboi.forward(50)
+smallboi.left(90)
+smallboi.forward(50)
+smallboi.left(180)
+smallboi.forward(100)
+#First letter
+##########################
+smallboi.penup()        #adjust position w/o writing
+smallboi.right(90)
+smallboi.forward(100)
+smallboi.pendown()     #resume writting
+smallboi.right(90)
+smallboi.forward(100)
+#finished second letter
+#############################
+wn.exitonclick()
