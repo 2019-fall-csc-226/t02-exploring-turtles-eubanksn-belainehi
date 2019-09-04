ef9cd6bc2e00cbe2823d4b63bab599a49a81c5e6
eubanksn <eubanksn@berea.edu>
2019-09-04 09:51:18 -0400

    Test

---
+++ TurtleEubanksn.py
@@ -0,0 +1,12 @@
+import turtle
+
+wn = turtle.Screen()
+wn.title("smallboi's small journey")
+wn.bgcolor("blue")
+smallboi = turtle.Turtle()
+smallboi.shape("turtle")
+smallboi.color("black")
+#everything to set up turtle and bg
+###################################
+
+
